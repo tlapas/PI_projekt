fc32a0e679b2b43c447f6bbede41ced5e3ac076c
Davor Vučina <davucina@gmail.com>
2013-05-19 08:55:52 +0200

    Baza v1.0
    Dodana je klasa za spajanje na bazu i rad s njom.
    Dodana je klasa artikli koja je spojena na bazu.

---
+++ Artikli.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_projekt
+{
+    public class Artikl
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Konstruktor za kreiranje artikla.
+        /// </summary>
+        public Artikl()
+        {
+            
+        }
+
+        /// <summary>
+        /// Puni objekt sa podacima iz DataReader objekta.
+        /// </summary>
+        /// <param name="dr">DataReader objekt sa podacima za Artikl.</param>
+        public Artikl(DbDataReader dr)
+        {
+            if (dr != null)
+            {
+                IdArtikla = int.Parse(dr["id_artikla"].ToString());
+                Naziv = dr["naziv"].ToString();
+                Cijena = float.Parse(dr["cijena"].ToString());
+                IdMjerneJedinice = int.Parse(dr["id_mjerne_jedinice"].ToString());
+            }
+
+        }
+
+        #endregion
+
+        #region Private Fields
+
+        private int id_artikla;
+        private string naziv;
+        private float cijena;
+        private int id_mjerne_jedinice;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Jedinstveni identifikator artikla.
+        /// </summary>
+        public int IdArtikla
+        {
+            get
+            {
+                return id_artikla;
+            }
+            private set
+            {
+                if (id_artikla != value)
+                {
+                    id_artikla = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Naziv artikla.
+        /// </summary>
+        public string Naziv
+        {
+            get
+            {
+                return naziv;
+            }
+            set
+            {
+                if (naziv != value)
+                {
+                    naziv = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Cijena artikla.
+        /// </summary>
+        public float Cijena
+        {
+            get
+            {
+                return cijena;
+            }
+            set
+            {
+                if (cijena != value)
+                {
+                    cijena = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Mjerna jedinica artikla (vanjski ključ).
+        /// </summary>
+        public int IdMjerneJedinice
+        {
+            get
+            {
+                return id_mjerne_jedinice;
+            }
+            private set
+            {
+                if (id_mjerne_jedinice != value)
+                {
+                    id_mjerne_jedinice = value;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Dohvaća sve artikle iz baze i vraća ih u obliku generičke liste.
+        /// </summary>
+        /// <returns>Lista artikala.</returns>
+        public static List<Artikl> DohvatiArtikle()
+        {
+            List<Artikl> lista = new List<Artikl>();
+            string sqlUpit = "SELECT * FROM Artikl";
+            DbDataReader dr = DB.Instance.DohvatiDataReader(sqlUpit);
+            while (dr.Read())
+            {
+                Artikl artikl = new Artikl(dr);
+                lista.Add(artikl);
+            }
+            dr.Close();     //Zatvaranje DataReader objekta.
+            return lista;
+        }
+
+        #endregion
+    }
+}
---
+++ Artikli/FrmArtikli.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PI_projekt.Artikli
+{
+    public partial class FrmArtikli : Form
+    {
+        public FrmArtikli()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmArtikli_Load(object sender, EventArgs e)
+        {
+            OsvjeziArtikle();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Dalje_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OsvjeziArtikle()
+        {
+            List<Artikl> listaArtikala = Artikl.DohvatiArtikle();
+            int brojac = 0;
+            foreach (Artikl Id in listaArtikala)
+            {
+                Naziv.Items.Add(listaArtikala[brojac].Naziv);
+                brojac++;
+            }
+        }
+
+        private void Naziv_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            List<Artikl> listaArtikala = Artikl.DohvatiArtikle();
+            if (Naziv.SelectedIndex == 0)
+                Cijena_box.Text = listaArtikala[0].Cijena.ToString() + " kn";
+            else if (Naziv.SelectedIndex == 1)
+                Cijena_box.Text = listaArtikala[1].Cijena.ToString() + " kn";
+            else if (Naziv.SelectedIndex == 2)
+                Cijena_box.Text = listaArtikala[2].Cijena.ToString() + " kn";
+            else if (Naziv.SelectedIndex == 3)
+                Cijena_box.Text = listaArtikala[3].Cijena.ToString() + " kn";
+            else if (Naziv.SelectedIndex == 4)
+                Cijena_box.Text = listaArtikala[4].Cijena.ToString() + " kn";
+            else if (Naziv.SelectedIndex == 5)
+                Cijena_box.Text = listaArtikala[5].Cijena.ToString() + " kn";
+        }
+    }
+}
