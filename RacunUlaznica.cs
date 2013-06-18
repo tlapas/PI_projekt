2cf7cd5a1a71404f10d3b2047e497a4beb0f22f0
Branimir Štivić <shtiva@hotmail.com>
2013-06-18 20:47:36 +0200

    RacunUlaznica.cs i Ulaznica.cs

---
+++ RacunUlaznica.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_projekt
+{
+    class RacunUlaznica
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Konstruktor za racuna ulaznice
+        /// </summary>
+        public RacunUlaznica()
+        {
+
+        }
+
+        /// <summary>
+        /// Puni objekt sa podacima iz DataReader objekta.
+        /// </summary>
+        /// <param name="dr">DataReader objekt sa podacima za film-Zanr.</param>
+        public RacunUlaznica(DbDataReader dr)
+        {
+            if (dr != null)
+            {
+                IdRacuna= int.Parse(dr["id_racuna"].ToString());
+                IdUlaznice= int.Parse(dr["id_karte"].ToString());
+                IdPopusta = int.Parse(dr["id_popusta"].ToString()); 
+            }
+
+        }
+
+        #endregion
+
+        #region Private Fields
+
+        private int id_racuna;
+        private int id_ulaznice;
+        private int id_popusta;
+     
+        #endregion
+
+        #region Properties
+
+
+        
+        /// <summary>
+        /// Jedinstveni identifikator ulaznice
+        /// </summary>
+        public int IdUlaznice
+        {
+            get
+            {
+                return id_ulaznice;
+            }
+            private set
+            {
+                if (id_ulaznice != value)
+                {
+                    id_ulaznice = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Jedinstveni identifikator projekcije
+        /// </summary>
+        public int IdRacuna
+        {
+            get
+            {
+                return id_racuna;
+            }
+            private set
+            {
+                if (id_racuna != value)
+                {
+                    id_racuna = value;
+                }
+            }
+        }
+        /// <summary>
+        /// broj sjedala
+        /// </summary>
+        public int IdPopusta
+        {
+            get
+            {
+                return id_popusta;
+            }
+            private set
+            {
+                if (id_popusta != value)
+                {
+                    id_popusta = value;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Dohvaća stavke svih ulaznica
+        /// </summary>
+        /// <returns>Lista stavki svih racun ulaznica</returns>
+        public static List<RacunUlaznica> DohvatiSveRacunUlaznica()
+        {
+            List<RacunUlaznica> lista = new List<RacunUlaznica>();
+            string sqlUpit = "SELECT * from racun_karta;";
+            DbDataReader dr = DB.Instance.DohvatiDataReader(sqlUpit);
+            while (dr.Read())
+            {
+                RacunUlaznica ulaznica = new RacunUlaznica(dr);
+                lista.Add(ulaznica);
+            }
+            dr.Close();     //Zatvaranje DataReader objekta.
+            return lista;
+        }
+
+        /// <summary>
+        /// Dohvaća stavke svih ulaznica za određeni račun
+        /// </summary>
+        /// <param name="idRacuna">Id računa za kojeg se dohvacaju sve stavke ulaznica</param>
+        /// <returns></returns>
+        public static List<RacunUlaznica> DohvatiSveRacunUlaznica(int idRacuna)
+        {
+            List<RacunUlaznica> lista = new List<RacunUlaznica>();
+            string sqlUpit = "SELECT * from racun_karta WHERE id_racuna="+idRacuna+";";
+            DbDataReader dr = DB.Instance.DohvatiDataReader(sqlUpit);
+            while (dr.Read())
+            {
+                RacunUlaznica ulaznica = new RacunUlaznica(dr);
+                lista.Add(ulaznica);
+            }
+            dr.Close();     //Zatvaranje DataReader objekta.
+            return lista;
+        }
+
+        
+        #endregion
+    }
+}
---
+++ Ulaznica.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_projekt
+{
+    class Ulaznice
+    {
+           
+        #region Constructors
+
+        /// <summary>
+        /// Konstruktor za kreiranje  vrste projekcije
+        /// </summary>
+        public Ulaznice()
+        {
+
+        }
+
+        /// <summary>
+        /// Puni objekt sa podacima iz DataReader objekta.
+        /// </summary>
+        /// <param name="dr">DataReader objekt sa podacima za film-Zanr.</param>
+        public Ulaznice(DbDataReader dr)
+        {
+            if (dr != null)
+            {
+                IdUlaznice = int.Parse(dr["id_karte"].ToString());
+                Sjedalo = int.Parse(dr["sjedalo"].ToString());
+                IdProjekcije = int.Parse(dr["id_projekcije"].ToString()); 
+            }
+
+        }
+
+        #endregion
+
+        #region Private Fields
+
+        private int id_ulaznice;
+        private int sjedalo;
+        private int id_projekcije;
+     
+        #endregion
+
+        #region Properties
+
+
+        
+        /// <summary>
+        /// Jedinstveni identifikator ulaznice
+        /// </summary>
+        public int IdUlaznice
+        {
+            get
+            {
+                return id_ulaznice;
+            }
+            private set
+            {
+                if (id_ulaznice != value)
+                {
+                    id_ulaznice = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Jedinstveni identifikator projekcije
+        /// </summary>
+        public int IdProjekcije
+        {
+            get
+            {
+                return id_projekcije;
+            }
+            private set
+            {
+                if (id_projekcije != value)
+                {
+                    id_projekcije = value;
+                }
+            }
+        }
+        /// <summary>
+        /// broj sjedala
+        /// </summary>
+        public int Sjedalo
+        {
+            get
+            {
+                return sjedalo;
+            }
+            private set
+            {
+                if (sjedalo != value)
+                {
+                    sjedalo = value;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Dohvaća sve ulaznice
+        /// </summary>
+        /// <returns>Lista svih ulaznica</returns>
+        public static List<Ulaznice>  DohvatiSveUlaznice()
+        {
+            List<Ulaznice> lista = new List<Ulaznice>();
+            string sqlUpit = "SELECT * from Karta ;";
+            DbDataReader dr = DB.Instance.DohvatiDataReader(sqlUpit);
+            while (dr.Read())
+            {
+                Ulaznice ulaznica = new Ulaznice(dr);
+                lista.Add(ulaznica);
+            }
+            dr.Close();     //Zatvaranje DataReader objekta.
+            return lista;
+        }
+
+       /// <summary>
+        /// Dohvaća sve ulaznice za određenu projekciju
+       /// </summary>
+       /// <param name="idProjekcije">Id projekcije za koju se dohvaćaju sve ulaznice</param>
+       /// <returns>vrća listu ulaznica</returns>
+        public static List<Ulaznice> DohvatiUlazniceProjekciju(int idProjekcije)
+        {
+            List<Ulaznice> lista = new List<Ulaznice>();
+            string sqlUpit = "SELECT Karta.* FROM Karta LEFT JOIN Projekcija ON Karta.id_projekcije=Projekcija.id_projekcije WHERE Projekcija.id_projekcije=" 
+                + idProjekcije + ";";
+            DbDataReader dr = DB.Instance.DohvatiDataReader(sqlUpit);
+            while (dr.Read())
+            {
+                Ulaznice ulaznica = new Ulaznice(dr);
+                lista.Add(ulaznica);
+            }
+            dr.Close();     //Zatvaranje DataReader objekta.
+            return lista;
+        }
+       
+
+        
+        #endregion
+    }
+}
