71859591c006cbd0b35122b57a04b74704c4999f
Davor Vučina <davucina@gmail.com>
2013-06-19 10:54:25 +0200

    Dodane klase: Racun.cs i RacunArtikli.cs
    Klase za rad s tablicama iz BP. Racun.cs (Racun), RacunArtikli.cs
    (stavke_racuna)

---
+++ Racun.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_projekt
+{
+    class Racun
+    {
+
+        #region Constructors
+
+        /// <summary>
+        /// Konstruktor za kreiranje računa
+        /// </summary>
+        public Racun()
+        {
+
+        }
+
+        /// <summary>
+        /// Puni objekt sa podacima iz DataReader objekta.
+        /// </summary>
+        /// <param name="dr">DataReader objekt sa podacima za račun.</param>
+        public Racun(DbDataReader dr)
+        {
+            if (dr != null)
+            {
+                IdRacuna = int.Parse(dr["id_racuna"].ToString());
+                OIB = float.Parse(dr["OIB"].ToString());
+                IdNacinaPlacanja = int.Parse(dr["id_nacina_placanja"].ToString());
+            }
+
+        }
+
+        #endregion
+
+        #region Private Fields
+
+        private int id_racuna;
+        private float oib;
+        private int id_nacina_placanja;
+
+        #endregion
+
+        #region Properties
+
+
+
+        /// <summary>
+        /// Jedinstveni identifikator računa
+        /// </summary>
+        public int IdRacuna
+        {
+            get
+            {
+                return id_racuna;
+            }
+            private set
+            {
+                if (id_racuna != value)
+                {
+                    id_racuna = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// OIB
+        /// </summary>
+        public float OIB
+        {
+            get
+            {
+                return oib;
+            }
+            private set
+            {
+                if (oib != value)
+                {
+                    oib = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Jedinstveni identifikator načina plačanja
+        /// </summary>
+        public int IdNacinaPlacanja
+        {
+            get
+            {
+                return id_nacina_placanja;
+            }
+            private set
+            {
+                if (id_nacina_placanja != value)
+                {
+                    id_nacina_placanja = value;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Dohvaća sve račune
+        /// </summary>
+        /// <returns>Lista svih računa</returns>
+        public static List<Racun> DohvatiSveRacune()
+        {
+            List<Racun> lista = new List<Racun>();
+            string sqlUpit = "SELECT * FROM Racun;";
+            DbDataReader dr = DB.Instance.DohvatiDataReader(sqlUpit);
+            while (dr.Read())
+            {
+                Racun racun = new Racun(dr);
+                lista.Add(racun);
+            }
+            dr.Close();
+            return lista;
+        }
+        
+        /// <summary>
+        /// Unos novog računa
+        /// </summary>
+        /// <param name="idRacuna">Id računa</param>
+        /// <returns>int vraća ID računa</returns>
+        public static int UnesiRacun(int OIB, int IDNacinaPlacanja)
+        {
+
+            string sqlUpit = "INSERT INTO Racun ('OIB','id_nacina_placanja') VALUES ('" + OIB + "','" + IDNacinaPlacanja + "');";
+            int idRacuna = DB.Instance.IzvrsiUpitID(sqlUpit);
+
+            return idRacuna;
+        }
+
+        #endregion
+    }
+}
---
+++ RacunArtikl.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_projekt
+{
+    class RacunArtikl
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Konstruktor za RacunArtikl
+        /// </summary>
+        public RacunArtikl()
+        {
+
+        }
+
+        /// <summary>
+        /// Puni objekt sa podacima iz DataReader objekta.
+        /// </summary>
+        /// <param name="dr">DataReader objekt sa podacima za stavke_racuna.</param>
+        public RacunArtikl(DbDataReader dr)
+        {
+            if (dr != null)
+            {
+                IdRacuna = int.Parse(dr["id_racuna"].ToString());
+                IdArtikla = int.Parse(dr["id_artikla"].ToString());
+                Kolicina = int.Parse(dr["kolicina"].ToString());
+            }
+
+        }
+
+        #endregion
+
+        #region Private Fields
+
+        private int id_racuna;
+        private int id_artikla;
+        private int kolicina;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Jedinstveni identifikator računa
+        /// </summary>
+        public int IdRacuna
+        {
+            get
+            {
+                return id_racuna;
+            }
+            private set
+            {
+                if (id_racuna != value)
+                {
+                    id_racuna = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Jedinstveni identifikator artikla
+        /// </summary>
+        public int IdArtikla
+        {
+            get
+            {
+                return id_artikla;
+            }
+            private set
+            {
+                if (id_artikla != value)
+                {
+                    id_artikla = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Količina (artikala)
+        /// </summary>
+        public int Kolicina
+        {
+            get
+            {
+                return kolicina;
+            }
+            private set
+            {
+                if (kolicina != value)
+                {
+                    kolicina = value;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Dohvaća stavke svih računa
+        /// </summary>
+        /// <returns>Lista stavki svih računa (artikli)</returns>
+        public static List<RacunArtikl> DohvatiSveRacunArtikl()
+        {
+            List<RacunArtikl> lista = new List<RacunArtikl>();
+            string sqlUpit = "SELECT * from stavka_racuna;";
+            DbDataReader dr = DB.Instance.DohvatiDataReader(sqlUpit);
+            while (dr.Read())
+            {
+                RacunArtikl artikl = new RacunArtikl(dr);
+                lista.Add(artikl);
+            }
+            dr.Close();
+            return lista;
+        }
+
+        /// <summary>
+        /// Dohvaća stavke (artikle) za određeni račun
+        /// </summary>
+        /// <param name="idRacuna">Id računa za kojeg se dohvaćaju sve stavke</param>
+        /// <returns></returns>
+        public static List<RacunArtikl> DohvatiSveRacunArtikl(int idRacuna)
+        {
+            List<RacunArtikl> lista = new List<RacunArtikl>();
+            string sqlUpit = "SELECT * from stavke_racuna WHERE id_racuna=" + idRacuna + ";";
+            DbDataReader dr = DB.Instance.DohvatiDataReader(sqlUpit);
+            while (dr.Read())
+            {
+                RacunArtikl artikl = new RacunArtikl(dr);
+                lista.Add(artikl);
+            }
+            dr.Close();
+            return lista;
+        }
+
+        #endregion
+    }
+}
